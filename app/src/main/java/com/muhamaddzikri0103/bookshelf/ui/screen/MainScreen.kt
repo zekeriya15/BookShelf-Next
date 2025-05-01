572ea62121d8884f85e1a96d61bcbe887eb63937
Muhamad Dzikriansyah <dzikriansyah1503@gmail.com>
2025-05-01 21:59:44 +0700

    Separate composable from activity

---
+++ app/src/main/java/com/muhamaddzikri0103/bookshelf/ui/screen/MainScreen.kt
@@ -0,0 +1,53 @@
+package com.muhamaddzikri0103.bookshelf.ui.screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.muhamaddzikri0103.bookshelf.R
+import com.muhamaddzikri0103.bookshelf.ui.theme.BookShelfTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Android!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BookShelfTheme {
+        MainScreen()
+    }
+}

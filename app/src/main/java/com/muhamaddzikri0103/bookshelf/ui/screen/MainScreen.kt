c777b6cb48bfe7e4eeb456b52e98c68834bd691b
Muhamad Dzikriansyah <dzikriansyah1503@gmail.com>
2025-05-02 00:34:09 +0700

    Handle state when data is empty

--- app/src/main/java/com/muhamaddzikri0103/bookshelf/ui/screen/MainScreen.kt
+++ app/src/main/java/com/muhamaddzikri0103/bookshelf/ui/screen/MainScreen.kt
@@ -16,9 +16,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,13 +54,28 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
+//    val data = emptyList<BookAndReading>()
 
-    LazyColumn(
-        modifier = modifier.fillMaxSize()
-    ) {
-        items(data) {
-            ListItem(bookNreading = it)
-            HorizontalDivider()
+    if (data.isEmpty()) {
+        Column(
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.empty_list),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+    else {
+        LazyColumn(
+            modifier = modifier.fillMaxSize()
+        ) {
+            items(data) {
+                ListItem(bookNreading = it)
+                HorizontalDivider()
+            }
         }
     }
 }
